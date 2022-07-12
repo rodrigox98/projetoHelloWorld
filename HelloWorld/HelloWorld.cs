04b83410a89f4f58f9f6d637ceaf52bda097c9eb
rodrigox98 <rodrigo.silva31@hotmail.com>
2022-07-12 16:41:38 -0300

    Lista 1

---
+++ HelloWorld/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            Console.WriteLine("Hello World! Turma 2 .NET");
+            
+            }
+        }
+    }
+
